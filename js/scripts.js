47c78510c69b20e2c54c069103f5bf7595862bfe
kimdodo9655 <kimdodo9655@gmail.com>
2023-03-03 14:41:02 +0900

    Append

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,4 @@
 $(document).ready(function () {
-  $(window).scroll(function () {
-    var scrollTop = $(this).scrollTop();
-    var windowWidth = $(window).width();
-
-    if (windowWidth < 700 || scrollTop > 30) {
-      $('.sign_area_pc').css('display', 'none');
-    } else {
-      $('.sign_area_pc').css('display', 'block');
-    }
-  });
-
-  $(window).resize(function () {
-    var windowWidth = $(window).width();
-
-    if (windowWidth > 700) {
-      $('.nav_area .inner ul').removeClass('active');
-      $('.menu-trigger').removeClass('active');
-      $('.sign_area_pc').css('display', 'block');
-    } else if (windowWidth <= 700) {
-      $('.sign_area_pc').css('display', 'none');
-    }
-  });
-
   $('.menu-trigger').each(function () {
     var $this = $(this);
 
