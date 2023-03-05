357363a8a7fdcae80c99ccbba59ec11718898822
kimdodo9655 <kimdodo9655@gmail.com>
2023-03-05 23:56:40 +0900

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //모바일 메뉴
   $('.menu-trigger').each(function () {
     var $this = $(this);
 
@@ -14,5 +15,28 @@ $(document).ready(function () {
     } else {
       $('.nav_area .inner ul').addClass('active');
     }
+  });
+
+  // 주소
+  $('.addr_btn').on('click', function () {
+    new daum.Postcode({
+      oncomplete: function (data) {
+        console.log(data);
+        var roadAddr = data.roadAddress;
+        var jibunAddr = data.jibunAddress;
+        $('#member_post').val(data.zonecode);
+        if (roadAddr !== '') {
+          $('#member_addr').val(roadAddr);
+        } else if (jibunAddr !== '') {
+          $('#member_addr').val(jibunAddr);
+        }
+      },
+    }).open();
+  });
+
+  $('.radio_btn').on('click', function () {
+    $('input[name="user_gender"]').val($(this).text());
+    $('.radio_btn').removeClass('active');
+    $(this).addClass('active');
   });
 });
